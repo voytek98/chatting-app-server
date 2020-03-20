03c9ea02d8a47a8b267615389294a14cba6582a5
wojciech-florczak <florczak@wojciech.dev>
2020-03-20 15:10:12 +0100

    Feat: handle "join" socket emit from client

--- index.js
+++ index.js
@@ -10,13 +10,17 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on("connection", (socket)=> {
-  console.log('new connection')
-  
-  socket.on('disconnect', ()=> {
-    console.log('user has left')
-  })
-})
+io.on("connection", socket => {
+  console.log("new connection");
+
+  socket.on("join", ({ name, room }) => {
+    console.log(`User with name ${name} has joined room ${room}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user has left");
+  });
+});
 
 app.use(router);
 
